6adf0c7d6ae5d792f1833aeb4395f161e72b71c5
duran.valentina@hotmail.com <duran.valentina@hotmail.com>
2022-09-30 18:28:54 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio75/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio75
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool verdad = true;
+            string Vacio = "";
+            Stack<string> pila = new Stack<string>();
+            while (verdad)
+            {
+                Console.WriteLine("Ingresa un dato alfanúmerico");
+                string Dato = Console.ReadLine();
+                if (Dato == Vacio)
+                {
+                    verdad = false;
+                }
+                else
+                {
+                    pila.Push(Dato);
+                }
+            }
+            while (pila.Count > 0)
+            {
+                string dato = pila.Pop();
+                Console.WriteLine(dato);
+            }
+        }
+    }
+}
